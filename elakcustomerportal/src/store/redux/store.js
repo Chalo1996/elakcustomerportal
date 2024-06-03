c1571baeab83e74d32ddefe9aedbb2723591fcef
Chalo1996 <emusyoka759@gmail.com>
2024-06-03 15:16:28 +0300

    fix reducer missing error

--- elakcustomerportal/src/store/redux/store.js
+++ elakcustomerportal/src/store/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import reducer from "../../reducers/reducer";
+
 const store = configureStore({
   reducer,
   devTools: process.env.NODE_ENV !== "production",
 });
 
-export default store
+export default store;
