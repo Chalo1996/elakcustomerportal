11a692555b9932cb1ec097932b57bd9a925ac161
Chalo1996 <emusyoka759@gmail.com>
2024-06-12 14:10:53 +0300

    Fix conflicts

--- elakcustomerportal/src/store/redux/store.js
+++ elakcustomerportal/src/store/redux/store.js
@@ -1,13 +1,14 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userDetailsReducer from "../../reducers/reducer";
+import authReducer from "../../reducers/authReducer";
+import funeralExpenseReducer from "../../reducers/funeralExpenseReducer";
 
 const rootReducer = combineReducers({
   userDetails: userDetailsReducer,
+  auth: authReducer,
+  funeralExpense: funeralExpenseReducer,
 });
 
 export const store = configureStore({
-  reducer: rootReducer: {
-    auth: authReducer,
-    funeralExpense: funeralExpenseReducer,
-  },
+  reducer: rootReducer,
 });
