ac59f556827e9369a77e34e97c3bf0bf4fb713a0
Ngala254 <antonyngala27@gmail.com>
2024-06-20 11:17:32 +0300

    Changes to the positioning of  Buttons in Group Life Quotation

--- elakcustomerportal/src/pages/GroupLifeAssurance/GroupLifeQuotation.js
+++ elakcustomerportal/src/pages/GroupLifeAssurance/GroupLifeQuotation.js
@@ -68,22 +68,22 @@ const QuotationTable = () => {
         premium: formatCurrency(benefit.premium)
       }))
     };
-  
+
     fetch(templateFile)
       .then((res) => res.arrayBuffer())
       .then((buffer) => {
         const zip = new PizZip(buffer);
         const doc = new Docxtemplater(zip);
-  
+
         doc.setData(docData);
-  
+
         try {
           doc.render();
         } catch (error) {
           console.error("Error rendering document:", error);
           return;
         }
-  
+
         const generatedDoc = doc.getZip().generate({ type: "blob" });
         FileSaver.saveAs(generatedDoc, "Quotation.docx");
       })
@@ -91,16 +91,16 @@ const QuotationTable = () => {
         console.error("Error loading template file:", error);
       });
   };
-  
+
 
   const location = useLocation();
-  const { data = {}, formData= {} } = location.state || {};
+  const { data = {}, formData = {} } = location.state || {};
 
   const proposedClientDetails = [
     { key: "nameOfClient", attribute: "Name of Client", value: data.proposedClientDetails.nameOfClient },
     { key: "totalAnnualSalaries", attribute: "Total Annual Salaries", value: formatCurrency(data.proposedClientDetails.totalAnnualSalaries) },
     { key: "totalNumberOfStaff", attribute: "Total Number of Staff", value: data.proposedClientDetails.totalNumberOfStaff },
-    { key: "mobileNumber", attribute: "Mobile Number", value: `${formData.phoneArea} ${formData.mobileNumber}`},
+    { key: "mobileNumber", attribute: "Mobile Number", value: `${formData.phoneArea} ${formData.mobileNumber}` },
     { key: "email", attribute: "Email Address", value: formData.email },
     { key: "glaAsMultipleofAnnualSalary", attribute: "GLA Benefit Level as Multiple of Annual Salary", value: data.gla.benefits[0].benefitLevel },
     { key: "criticalIllnessBenefitPercentage", attribute: "Critical Illness Benefit Percentage", value: `${data.criticalIllnessBenefitPercentage}%` },
@@ -147,12 +147,12 @@ const QuotationTable = () => {
   return (
     <div className="pt-5 pl-4">
       <div className="flex items-center" style={{ marginBottom: "20px", marginTop: "10px" }} >
-          <button className="mb-1.5 focus:outline-none hover:text-[#A32A29]" >
-            <LeftOutlined className="w-8 h-8" onClick={handleNavigate}/>
-          </button>
-          <Title level={5} className="font-open-sans text-[16px] font-semibold leading-[24px] text-left">
-            Group Life Assurance Cover
-          </Title>
+        <button className="mb-1.5 focus:outline-none hover:text-[#A32A29]" >
+          <LeftOutlined className="w-8 h-8" onClick={handleNavigate} />
+        </button>
+        <Title level={5} className="font-open-sans text-[16px] font-semibold leading-[24px] text-left">
+          Group Life Assurance Cover
+        </Title>
       </div>
       <Card style={{ border: "1px solid maroon" }}>
         <div style={{ width: "90%", margin: "auto" }}>
@@ -261,30 +261,34 @@ const QuotationTable = () => {
           <h3 style={{ color: "white", margin: 0 }}><strong>Equity Life Assurance (Kenya) Limited</strong></h3>
         </div>
       </Card>
-      <div style={{ marginTop: "20px" }}>
-        <Checkbox checked={isCheckboxChecked} onChange={handleCheckboxChange} >
-          I accept the{" "}
-          <span onClick={() => setIsPolicyModalVisible(true)} style={{ textAlign: "right", marginTop: "20px", color: "#A32A29" }}>
-            policy exclusions
-          </span>
-        </Checkbox>
-
-        <div style={{ textAlign: "right", marginTop: "20px", color: "#A32A29" }}>
-          <Button
-            type="primary"
-            style={{ marginRight: "10px" }}
-            disabled={!isPolicyAccepted || !isCheckboxChecked}
-          >
-            Continue with Payment
-          </Button>
-          <Button className="mr-4" style={{ marginRight: "10px" }} onClick={handleDownload}>
-            Download
-          </Button>
-          <Button className="mr-4" style={{ marginRight: "10px" }}>
-            Send to Email
-          </Button>
-        </div>
-
+      <div style={{ marginTop: "30px" }}>
+        <Row>
+          <Col span={12} style={{ marginTop: "4px"}}>
+            <Checkbox checked={isCheckboxChecked} onChange={handleCheckboxChange} >
+              I accept the{" "}
+              <span onClick={() => setIsPolicyModalVisible(true)} style={{ textAlign: "right", marginTop: "20px", color: "#A32A29" }}>
+                policy exclusions
+              </span>
+            </Checkbox>
+          </Col>
+          <Col span={12}>
+            <div style={{ textAlign: "right", color: "#A32A29"}}>
+              <Button
+                type="primary"
+                style={{ marginRight: "10px" }}
+                disabled={!isPolicyAccepted || !isCheckboxChecked}
+              >
+                Continue with Payment
+              </Button>
+              <Button className="mr-4" style={{ marginRight: "10px" }} onClick={handleDownload}>
+                Download
+              </Button>
+              <Button className="mr-4" style={{ marginRight: "10px" }}>
+                Send to Email
+              </Button>
+            </div>
+          </Col>
+        </Row>
         <PolicyExclusionsModal
           visible={isPolicyModalVisible}
           onCancel={() => setIsPolicyModalVisible(false)}
