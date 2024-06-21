16dfbece8a7ecc155461dcb8fbe51663ee33b183
mogakaowen <owenmogaka@gmail.com>
2024-06-21 10:44:07 +0300

    set error page

--- elakcustomerportal/src/pages/AnnuityPages/Annuity.js
+++ elakcustomerportal/src/pages/AnnuityPages/Annuity.js
@@ -11,6 +11,7 @@ import ProductParametersForm from "../../components/Annuity/ProductParameters";
 import CoverageForm from "../../components/Annuity/Coverage";
 import ConfirmDetailsForm from "../../components/Annuity/ConfirmDetails";
 import axios from "axios";
+import ErrorPage from "../../shared/ErrorPage";
 
 const { Step } = Steps;
 
@@ -59,6 +60,7 @@ const AnnuityPage = () => {
   const token = useSelector((state) => state.auth.token);
   const [current, setCurrent] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const [formData, setFormData] = useState(() => {
     const initialData = getInitialFormData();
     return {
@@ -187,6 +189,7 @@ const AnnuityPage = () => {
         Authorization: `Bearer ${token}`,
       },
     });
+
     console.log("annuity response: ", response);
     return response.data.outData;
   };
@@ -198,6 +201,7 @@ const AnnuityPage = () => {
       await Promise.all(forms.map((form) => form.validateFields()));
       tableData = await fetchAnnuityData();
     } catch (error) {
+      setIsError(true);
       console.log("Validation Failed:", error);
     } finally {
       setIsLoading(false);
@@ -244,6 +248,17 @@ const AnnuityPage = () => {
       content: <ConfirmDetailsForm formData={formData} />,
     },
   ];
+
+  if (isError) {
+    return (
+      <ErrorPage
+        status="error"
+        title="Quote Generation Failed!"
+        subtitle="Sorry, there was an issue generating a quotation. Please try again later."
+        onRetry={() => window.location.reload()}
+      />
+    );
+  }
 
   return (
     <div className="pt-5 pl-4">
